86c660fb15152698f58b003e325efa3c27da2a6a
the-abdulbasit <mactisab@gmail.com>
2026-01-27 10:54:21 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Analytics } from "@vercel/analytics/react";
 import PortfolioWebVersion from './imports/PortfolioWebVersion';
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-[#050505]">
       <PortfolioWebVersion />
+      <Analytics />
     </div>
   );
 }
