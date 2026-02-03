13e1575ab43d3f9bdbd78dfe3b84cba31660a325
the-abdulbasit <mactisab@gmail.com>
2026-02-03 23:03:37 +0000

    Update AboutSection.tsx

--- src/imports/AboutSection.tsx
+++ src/imports/AboutSection.tsx
@@ -6,7 +6,7 @@ import AcademicBreakPopUp from "./AcademicBreakPopUp";
 function NameTitle() {
   return (
     <div className="content-stretch flex flex-col gap-[4px] items-start justify-center not-italic relative shrink-0" data-name="Name & Title">
-      <p className="font-['Inter',sans-serif] font-semibold leading-[1.45] relative shrink-0 text-2xl text-white tracking-[-0.72px]">Basit Ogunlana</p>
+      <p className="font-['Inter',sans-serif] font-semibold leading-[1.56] relative shrink-0 text-24 text-white tracking-[-0.75px]">Basit Ogunlana</p>
       <p className="font-['Inter',sans-serif] font-medium leading-[1.45] relative shrink-0 text-[#f5f5f5] text-[16px]">
         <span>Product Designer, </span>
         <span className="text-[#707070]">currently building GreshFi</span>
