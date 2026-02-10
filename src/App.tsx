8e45c2acf0e0d14c6f564756fd9badd92525bac2
the-abdulbasit <mactisab@gmail.com>
2026-02-10 16:47:13 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from '@vercel/analytics/react';
 import PortfolioWebVersion from './imports/PortfolioWebVersion';
-import ScrollIndicator from "./components/ScrollIndicator";
 
 export default function App() {
   return (
@@ -9,7 +8,6 @@ export default function App() {
       <PortfolioWebVersion />
       <Analytics />
       <SpeedInsights />
-      <ScrollIndicator />
     </div>
   );
 }
