839cba57a2def74b673eac3f4e217989a83fbc76
the-abdulbasit <mactisab@gmail.com>
2026-02-10 22:06:43 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import PortfolioWebVersion from './imports/PortfolioWebVersion';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-[#121212]">
+    <div className="min-h-screen bg-[#121212] text-[#e5e5e5]">
       <PortfolioWebVersion />
     </div>
   );
