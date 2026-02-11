a2d5b3a5b1f79c5d8ebf77543477f3b6c742a7ef
the-abdulbasit <mactisab@gmail.com>
2026-02-11 10:12:53 +0000

    Update AboutSection.tsx

--- src/imports/AboutSection.tsx
+++ src/imports/AboutSection.tsx
@@ -21,7 +21,7 @@ function About() {
 
   return (
     <div className="content-stretch flex flex-col font-['Inter',sans-serif] font-normal gap-[20px] items-start not-italic relative shrink-0 text-[#e5e5e5] text-[15px] w-full" data-name="About">
-      <p className="leading-[1.45] relative shrink-0 w-full">I surmise that the most satisfying interactions occur when sparks of curiosity meets memorable experiences—the allure of design as I like to call it.</p>
+      <p className="leading-[1.45] relative shrink-0 w-full">Designers often say the best designs are subtle — I believe they are not just subtle, rather they engage the user's curiosity and impress.</p>
       <div className="leading-[1.45] relative shrink-0 w-full">
         <span>I enjoy being in the middle of what it takes to achieve that. From initial sketches to shipped digital products, I craft intuitive user experiences through design and </span>
         <span 
